995c2a4dcd2c80c1382921ae10e1a7877b8fd642
Adam Nowak <lubieniebieski@gmail.com>
2024-03-31 20:13:20 +0200

    add commentary field to links

--- lib/link.rb
+++ lib/link.rb
@@ -1,9 +1,9 @@
-Link = Struct.new(:url, :title, :description, :source, :timestamp, :source_url, :tags, keyword_init: true) do
-  def initialize(url:, timestamp:, title: nil, description: nil, source: nil, source_url: nil, tags: [])
+Link = Struct.new(:url, :title, :description, :source, :timestamp, :source_url, :tags, :commentary, keyword_init: true) do
+  def initialize(url:, timestamp:, title: nil, description: nil, source: nil, source_url: nil, tags: [], commentary: nil)
     timestamp = DateTime.parse(timestamp.to_s).to_datetime.to_s
     tags |= [source]
 
-    super(url:, title:, description:, source:, timestamp:, source_url:, tags:)
+    super(url:, title:, description:, source:, timestamp:, source_url:, tags:, commentary: nil)
   end
 
   def id
@@ -12,13 +12,14 @@ Link = Struct.new(:url, :title, :description, :source, :timestamp, :source_url, 
 
   def self.from_hash(hash)
     new(
-      url: hash.fetch('url'),
-      title: hash.fetch('title'),
-      description: hash.fetch('description'),
-      source: hash.fetch('source'),
-      timestamp: hash.fetch('timestamp'),
-      source_url: hash.fetch('source_url'),
-      tags: hash.fetch('tags')
+      url: hash.fetch("url"),
+      title: hash.fetch("title"),
+      description: hash.fetch("description"),
+      source: hash.fetch("source"),
+      timestamp: hash.fetch("timestamp"),
+      source_url: hash.fetch("source_url"),
+      tags: hash.fetch("tags"),
+      commentary: hash.fetch("commentary")
     )
   end
 end
